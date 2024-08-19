b47d007fed078c5ea5b6f05c14095e1128c276bc
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-08-19 11:09:04 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+import { View, Button, StyleSheet, TextInput, Alert, FlatList,  } from 'react-native';
+import { useState } from 'react';
+import { usarBD } from './hooks/usarBD';
+import { Produto } from './components/produto';
+import React, { useEffect} from 'react';
+
+
+
+export function Index() {
+    const [id, setId] = useState('');
+    const [nome, setNome] = useState('');
+    const [quantidade, setQuantidade] = useState('');
+    const [pesquisa, setPesquisa] = useState('');
+    const [produtos, setProdutos] = useState([]);
+
+    const produtosBD = usarBD();
+
+    async function create() {
+        if (isNaN(quantidade)) {
+            return Alert.alert('Quantidade', 'A quantidade precisa ser um número!');
+        }
+        try {
+            const item = await produtosBD.create({
+                nome,
+                quantidade: Number(quantidade),
+            });
+            Alert.alert('Produto cadastrado com o ID: ' + item.idProduto);
+            setId(item.idProduto);
+            listar();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function listar() {
+        try {
+            const captura = await produtosBD.read(pesquisa)
+            setProdutos(captura)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        listar();
+    }, [pesquisa]);
+
+    const remove = async (id) => {
+        try {
+            await produtosBD.remove(id);
+            await listar();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <TextInput style={styles.texto} placeholder="Nome" onChangeText={setNome} value={nome} />
+            <TextInput style={styles.texto} placeholder="Quantidade" onChangeText={setQuantidade} value={quantidade} />
+            <Button title="Salvar" onPress={create} />
+            <TextInput style={styles.texto} placeholder="Pesquisar" onChangeText={setPesquisa}/>
+            <FlatList
+                contentContainerStyle={styles.listContent}
+                data={produtos}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={({ item }) => (
+                    <Produto data={item} onDelete={() => remove(item.id)} /> 
+                )}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 32,
+        gap: 16,
+    },
+    texto: {
+        height: 54,
+        borderWidth: 1,
+        borderRadius: 7,
+        borderColor: "#999",
+        paddingHorizontal: 16,
+    },
+    listContent: {
+        gap: 16,
+    },
+});
